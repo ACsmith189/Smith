bfb7200d456c878130c2e1e845684666763d6c98
AndrewCSmith1 <116054076+ACsmith189@users.noreply.github.com>
2022-11-03 03:18:05 +0000

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-alert("welcome, this is a javascript popup alert");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function clearStorage()
-    var answer list = document.getElementById(element)
+function submitForm(name, hometown) {
+  //search the dom for a specific tag with the id "guestlist"
+  var guestlist = document.getElementById( "guestlist");
+  var n = document.getElementById( "name");
+  var h = document. getElementById( "hometown");
+  guestlist.innerHTML += "<li>" + name + " + hometown + "«/li>";
+  n.value = "";
+  h.value = "";
+  n. focus();
+  return false;
+}
+  
